930f9a047ddfdf2fa2b01e1806827833cacc880d
Thanh Ha <thanh3203@gmail.com>
2021-11-10 15:03:56 -0800

    Implemented code for .push, .pop, and .size for Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = -1;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
+    storage[counter] = value;
+
   };
 
   someInstance.pop = function() {
+    delete storage[counter];
+    counter--;
   };
 
   someInstance.size = function() {
+    var result = Object.keys(storage);
+    return result.length;
   };
 
   return someInstance;
