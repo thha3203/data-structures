0e21145ef13e044e73b12ba062b1de0889448766
Thanh Ha <thanh3203@gmail.com>
2021-11-10 15:20:30 -0800

    Implemented code into .enqueue, .dequeue, and .size to work the way it is intended with all passing tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = -1;
+  var beginning = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+    let result = storage[beginning];
+    delete storage[beginning];
+    beginning++;
+    return result;
   };
 
   someInstance.size = function() {
+    let result = Object.keys(storage);
+    return result.length;
   };
 
   return someInstance;
