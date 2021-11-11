2a7120cd759f5e4ce30beea8429d0610513ac7ca
Thanh Ha <thanh3203@gmail.com>
2021-11-10 16:40:28 -0800

    Implemented enqueue, dequeue, size methods in queueMethods and extended it to an Object that is being constructed in the Que constructor

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = -1;
+  someInstance.begin = 0;
+
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  dequeue: function() {
+    let result = this.storage[this.begin];
+    delete this.storage[this.begin];
+    this.begin++;
+    return result;
+  },
+  size: function() {
+    let result = Object.keys(this.storage);
+    return result.length;
+  }
+};
 
 
