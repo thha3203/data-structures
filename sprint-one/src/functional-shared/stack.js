8bb408ee65f4d9469d1c1a9772f78e3f361069b7
Thanh Ha <thanh3203@gmail.com>
2021-11-10 16:28:19 -0800

    Implemented push, pop, size functions into stackMethods and extended stackMethods into an instance that is created within Stack Class

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = -1;
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  pop: function() {
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return result;
+  },
+  size: function() {
+    let result = Object.keys(this.storage);
+    return result.length;
+  }
+};
 
 
