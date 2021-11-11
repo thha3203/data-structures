7c34661d7d58a1c8e2ec5b7f0461f650f1ed171d
Thanh Ha <thanh3203@gmail.com>
2021-11-10 17:19:00 -0800

    Used ES6 class declaration to implement push, pop, and size into a constructor.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = -1;
+  }
+
+  push(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  pop() {
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return result;
+  }
+
+  size() {
+    let result = Object.keys(this.storage);
+    return result.length;
   }
 
 }
