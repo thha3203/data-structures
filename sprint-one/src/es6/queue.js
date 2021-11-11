a60cd37d5995ef4b867fb4319af4f611b313a18f
Thanh Ha <thanh3203@gmail.com>
2021-11-10 17:22:17 -0800

    Used ES6 class declaration to implement enqueue, dequeue, and size to constructor.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = -1;
+    this.beginning = 0;
+  }
+
+  enqueue(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  dequeue() {
+    let result = this.storage[this.beginning];
+    delete this.storage[this.beginning];
+    this.beginning++;
+    return result;
+  }
+
+  size() {
+    let result = Object.keys(this.storage);
+    return result.length;
   }
 
 }
