81b83f1a62a184c7acf37d38d184fe6d273166cf
Thanh Ha <thanh3203@gmail.com>
2021-11-10 17:12:23 -0800

    Implemented enqueue, dequque, size using pseudo-classical inheritance for Queue Class.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = -1;
+  this.beginning = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  let result = this.storage[this.beginning];
+  delete this.storage[this.beginning];
+  this.beginning++;
+  return result;
+};
+
+Queue.prototype.size = function() {
+  let result = Object.keys(this.storage);
+  return result.length;
+};
