a841910d3eeb8df55d1acac9b01f242f82ea307e
Thanh Ha <thanh3203@gmail.com>
2021-11-10 17:00:27 -0800

    Created an instance with a fallback to queuekMethods for Queue Class. Implemented enqueue, dequeue, and size in queueMethods.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Create and object and set it's fallback source to stackMethods
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.count = -1;
+  someInstance.beginning = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  dequeue: function() {
+    let result = this.storage[this.beginning];
+    delete this.storage[this.beginning];
+    this.beginning++;
+    return result;
+  },
+  size: function() {
+    let result = Object.keys(this.storage);
+    return result.length;
+  }
+};
 
 
