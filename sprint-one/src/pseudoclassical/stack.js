6dbbbf4ff3e30e45aa990740f81ba4bb83da1eeb
Thanh Ha <thanh3203@gmail.com>
2021-11-10 17:07:48 -0800

    Implemented push, pop, size functions using pseudo-classical inheritance to Stack Class.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = -1;
 };
 
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Stack.prototype.pop = function() {
+  let result = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return result;
+};
+
+Stack.prototype.size = function() {
+  let result = Object.keys(this.storage);
+  return result.length;
+};
